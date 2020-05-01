a2c845b9cbefab200eb9dcf7fee81c9216d8540d
M3L6H <sisoac@hotmail.com>
2020-05-01 09:53:24 -0700

    Updated counting sort to use a block

--- Ruby/counting_sort.rb
+++ Ruby/counting_sort.rb
@@ -1,32 +1,33 @@
 # Time complexity is O(n + k) where k is the range of possible values
 
 class Array
-    def counting_sort!
+    # Accepts an optional block to map the contents of the array into the range
+    # (-9..9)
+    def counting_sort(&prc)
+        prc ||= Proc.new { |num| num }
+        
         counts = Array.new(19, 0)
 
         # Counting pass
         self.each do |num|
-            raise ArgumentError, "num should be numeric!" unless Numeric === num
-            raise ArgumentError "|#{num}| should be less than 10" unless num.abs < 10
-            counts[num + 9] += 1
+            raise ArgumentError, "#{prc.call(num)} should be numeric!" unless Numeric === prc.call(num)
+            raise ArgumentError "|#{prc.call(num)}| should be less than 10" unless prc.call(num).abs < 10
+            counts[prc.call(num) + 9] += 1
         end
 
         # Summing pass
         (-8..9).each { |i| counts[i+9] += counts[i + 8] }
 
         # Re-writing array
-        num = 0
-        (0...self.size).each do |i| 
-            num += 1 while i + 1 > counts[num]
-            self[i] = num - 9
+        new_arr = Array.new(self.size)
+        (self.size - 1).downto(0) do |i|
+            counts[prc.call(self[i]) + 9] -= 1
+            new_arr[counts[prc.call(self[i]) + 9]] = self[i]
         end
 
-        # Return
-        self
-    end
 
-    def counting_sort
-        self.dup.counting_sort!
+        # Return
+        new_arr
     end
 end
 
@@ -36,4 +37,7 @@ if $PROGRAM_NAME == __FILE__
     
     arr = [-1, 3, -3, 2, -2, 3, -1, 1, -2, 3, -3, -3]
     p arr.counting_sort
+    
+    arr = [10, 14, 15, 13, 13, 12, 15, 14, 13, 14, 13, 12, 13]
+    p arr.counting_sort { |num| num % 10 }
 end
