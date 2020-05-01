8dc1d7a106606e15dcbd5f5ef53e5b49017c9b87
M3L6H <sisoac@hotmail.com>
2020-05-01 10:09:44 -0700

    Updated radix_sort to no longer monkey patch the Array class

--- Ruby/radix_sort.rb
+++ Ruby/radix_sort.rb
@@ -1,19 +1,17 @@
 require_relative "./counting_sort.rb"
 
-class Array
-    def radix_sort
-        max = self.max
-        sorted = self
-        i = 0
-        while max / (10 ** i) > 0
-            sorted = sorted.counting_sort { |num| (num / (10 ** i)) % 10 }
-            i += 1
-        end
-        sorted
+def radix_sort(arr)
+    max = arr.max
+    sorted = arr
+    i = 0
+    while max / (10 ** i) > 0
+        sorted = sorted.counting_sort { |num| (num / (10 ** i)) % 10 }
+        i += 1
     end
+    sorted
 end
 
 if $PROGRAM_NAME == __FILE__
     arr = [1,4,12,511,25,12,515,3252,362,32,3,26535]
-    p arr.radix_sort
+    p radix_sort(arr)
 end
