6e797f88faa3d12f285ebeb02a9e055b57d70514
M3L6H <sisoac@hotmail.com>
2020-05-02 14:26:14 -0700

    Updated bubble sort to use std::swap

--- Cpp/bubble_sort.cpp
+++ Cpp/bubble_sort.cpp
@@ -18,9 +18,7 @@ vector<int> bubble_sort(vector<int>& nums) {
 
         for (int i = 0; i < nums.size() - 1; ++i) {
             if (nums[i] > nums[i + 1]) {
-                int temp = nums[i];
-                nums[i] = nums[i + 1];
-                nums[i + 1] = temp;
+                swap(nums[i], nums[i + 1]);
                 sorted = false;
             }
         }
