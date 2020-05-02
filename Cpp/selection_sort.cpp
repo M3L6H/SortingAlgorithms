7937302b29ce4c2a28e170d8b7a764158249147b
M3L6H <sisoac@hotmail.com>
2020-05-02 14:58:30 -0700

    Coded selection sort

---
+++ Cpp/selection_sort.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
+using namespace std;
+
+vector<int> selection_sort(vector<int>& nums) {
+    for (int i = 0; i < nums.size() - 1; ++i) {
+        int min_num = nums[i];
+        int min_idx = i;
+
+        for (int j = i; j < nums.size(); ++j) {
+            if (nums[j] < min_num) {
+                min_num = nums[j];
+                min_idx = j;
+            }
+        }
+
+        swap(nums[i], nums[min_idx]);
+    }
+
+    return nums;
+}
+
+void test_case() {
+    int n;
+    scanf("%d", &n);
+    vector<int> inp(n), ans(n);
+    for (int i = 0; i < n; ++i) scanf("%d", &inp[i]);
+    for (int i = 0; i < n; ++i) scanf("%d", &ans[i]);
+    assert(selection_sort(inp) == ans);
+}
+
+int main() {
+    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    int t;
+    scanf("%d", &t);
+    for (int i = 0; i < t; ++i) {
+        test_case();
+        printf(ANSI_COLOR_GREEN "Test %d passed.\n" ANSI_COLOR_RESET, i + 1);
+    }
+    return 0;
+}
