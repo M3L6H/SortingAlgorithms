86796941843f9bb3632338569485e1f640ef78ff
Michael Hollingworth <8094643+M3L6H@users.noreply.github.com>
2025-09-02 22:37:41 -0500

    chore(Bash/merge_sort): flesh out testing harness

--- Bash/merge_sort.test.sh
+++ Bash/merge_sort.test.sh
@@ -2,6 +2,33 @@
 
 merge_sort="$(dirname "$0")/merge_sort.sh"
 
-# empty case
-actual="$(merge_sort 0)"
-empty_case="$([ "$actual" = '' ])"
+chmod +x "$merge_sort"
+
+alias merge_sort="$merge_sort"
+
+passed=0
+failed=0
+
+# Evaluate the results of a test:
+#    $1: name - test name
+#    $2: actual - value produced by subject
+#    $3: expected - expected valud
+function eval_test {
+    local name="$1"
+    local actual="$2"
+    local expected="$3"
+    echo -n "${name}: "
+    if [ "$actual" = "$expected" ]; then
+        passed=$((passed + 1))
+        echo 'passed'
+    else
+        failed=$((failed + 1))
+        echo 'failed'
+    fi
+}
+
+eval_test 'empty case' "$(merge_sort 0)" ''
+
+echo "Passed: ${passed}"
+echo "Failed: ${failed}"
+exit "$([ failed -eq 0 ])"
