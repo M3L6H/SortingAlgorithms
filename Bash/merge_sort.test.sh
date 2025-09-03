6f606004a025e314e064b5bc909dcd374a8c1009
Michael Hollingworth <8094643+M3L6H@users.noreply.github.com>
2025-09-03 05:21:26 -0500

    chore(bash/merge_sort): add more tests

--- Bash/merge_sort.test.sh
+++ Bash/merge_sort.test.sh
@@ -32,6 +32,27 @@ function eval_test {
 }
 
 eval_test 'empty case' "$(merge_sort 0)" ''
+eval_test 'single case' "$(merge_sort 1 22)" '22'
+eval_test 'two elements' "$(merge_sort 2 8 3)" '3 8'
+eval_test 'three elements' "$(merge_sort 3 9 1 5)" '1 5 9'
+eval_test 'already sorted' "$(merge_sort 5 5 12 47 51 88)" '5 12 47 51 88'
+eval_test 'reversed' "$(merge_sort 6 103 95 40 38 23 19)" '19 23 38 40 95 103'
+eval_test 'swapped' "$(merge_sort 8 203 150 245 162 222 155 270 187)" '150 155 162 187 203 222 245 270'
+eval_test 'same' "$(merge_sort 16 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2" '2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2'
+
+function rev_comp {
+  local a="$1" b="$2"
+
+  if [ "$a" -lt "$b" ]; then
+    echo -n '1'
+  elif [ "$a" -gt "$b" ]; then
+    echo -n '-1'
+  else
+    echo -n '0'
+  fi
+}
+
+eval_test 'custom' "$(merge_sort 8 203 150 245 162 222 155 270 187 rev_comp)" '270 245 222 203 187 162 155 150'
 
 echo "Passed: ${passed}"
 echo "Failed: ${failed}"
