0ccee77cd2516d87875c35fe022f62bbd1cb9a6e
Michael Hollingworth <8094643+M3L6H@users.noreply.github.com>
2025-09-04 21:16:15 -0500

    fix(bash/counting_sort): catch non-zero exit status

--- Bash/counting_sort.test.sh
+++ Bash/counting_sort.test.sh
@@ -14,25 +14,34 @@ passed=0
 failed=0
 
 # Evaluate the results of a test:
-#    $1: name - test name
-#    $2: actual - value produced by subject
-#    $3: expected - expected valud
+#    1: name - test name
+#    2: expected - expected value
+#    3: cmd - command under test
+#    4: args to command
 function eval_test {
-    local name="$1"
-    local actual="$2"
-    local expected="$3"
+    local name="$1"; shift
+    local expected="$1"; shift
+    local cmd="$1"; shift
+    local actual="$(cmd "$@")"
+    
+    if [ "$?" -ne 0 ]; then
+        failed=$((failed+1))
+        echo 'error'
+        return
+    fi
+    
     echo -n "${name}: "
     if [ "$actual" = "$expected" ]; then
-        passed=$((passed + 1))
+        passed=$((passed+1))
         echo 'passed'
     else
-        failed=$((failed + 1))
+        failed=$((failed+1))
         echo 'failed'
     fi
 }
 
-eval_test 'empty' "$(c_sort 0)" ''
-eval_test 'single' "$(c_sort 1 5)" '5'
-eval_test 'double' "$(c_sort 2 7 2)" '2 7'
-eval_test 'same' "$(c_sort 3 8 8 8)" '8 8 8'
-eval_test 'big case' "$(c_sort 10 12 12 -4 -7 -4 3 8 12 4 5)" '-7 -4 -4 3 4 5 8 12 12 12'
+eval_test 'empty' '' c_sort 0
+eval_test 'single' '5' c_sort 1 5
+eval_test 'double' '2 7' c_sort 2 7 2
+eval_test 'same' '8 8 8' c_sort 3 8 8 8
+eval_test 'big case' '-7 -4 -4 3 4 5 8 12 12 12' c_sort 10 12 12 -4 -7 -4 3 8 12 4 5
