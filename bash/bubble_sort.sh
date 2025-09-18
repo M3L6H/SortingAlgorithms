c2740810cd3a5445f725ed973d8c285160499770
m3l6h <8094643+M3L6H@users.noreply.github.com>
2025-09-18 12:44:39 -0500

    fix(bash/bubble_sort): update prev & next appropriately

--- bash/bubble_sort.sh
+++ bash/bubble_sort.sh
@@ -21,28 +21,37 @@ function comp {
 # 3: Function to compare with
 function bubble_sort {
   local arr=()
-  local len="$1"; shift
-  while [ "$len" -gt 0 ]; do arr+=( "$1" ); len=$((len-1)); shift; done
+  local len="$1"
+  shift
+  while [ "$len" -gt 0 ]; do
+    arr+=("$1")
+    len=$((len - 1))
+    shift
+  done
   comp="$1"
-  
+
   local len="${#arr[@]}"
   [ "$len" -le 1 ] && echo "${arr[@]}" && return 0
   [ -z "$comp" ] && comp='comp'
 
   local sorted='false'
-  local prev=''
-  local next=''
 
   while ! "$sorted"; do
     sorted='true'
+
+    local prev=''
+    local next=''
+
     for i in "${!arr[@]}"; do
       prev="$next"
       next="${arr[i]}"
+
       [ -z "$prev" ] && continue
-      res="$("$comp" "$prev" "$next")"
-      if [ "$res" -gt 0 ]; then
-        arr[i-1]="$next"
+
+      if [ "$("$comp" "$prev" "$next")" -gt 0 ]; then
+        arr[i - 1]="$next"
         arr[i]="$prev"
+        next="$prev"
         sorted='false'
       fi
     done
@@ -51,4 +60,4 @@ function bubble_sort {
   echo "${arr[@]}"
 }
 
-[ "$_" != "$0" ] || bubble_sort "$@"
+bubble_sort "$@"
